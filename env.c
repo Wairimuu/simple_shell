16394177521956624b7ac7b97a882de86f6e9103
Wairimuu <xtynmaria@students.uonbi.ac.ke>
2023-11-15 02:15:47 -0800

    trial

--- env.c
+++ env.c
@@ -1,80 +1,31 @@
 #include "simple_shell.h"
 
-/**
- * cmp_env_name - compares env variables names
- * with the name passed.
- * @nenv: name of the environment variable
- * @name: name passed
- *
- * Return: 0 if are not equal. Another value if they are.
- */
-int cmp_env_name(const char *nenv, const char *name)
-{
-	int i;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-	for (i = 0; nenv[i] != '='; i++)
-	{
-		if (nenv[i] != name[i])
-		{
-			return (0);
-		}
-	}
+#define MAX_CMD_LEN 256
 
-	return (i + 1);
-}
+extern char **environ;
 
-/**
- * _getenv - get an environment variable
- * @name: name of the environment variable
- * @_environ: environment variable
- *
- * Return: value of the environment variable if is found.
- * In other case, returns NULL.
- */
-char *_getenv(const char *name, char **_environ)
-{
-	char *ptr_env;
-	int i, mov;
-
-	/* Initialize ptr_env value */
-	ptr_env = NULL;
-	mov = 0;
-	/* Compare all environment variables */
-	/* environ is declared in the header file */
-	for (i = 0; _environ[i]; i++)
-	{
-		/* If name and env are equal */
-		mov = cmp_env_name(_environ[i], name);
-		if (mov)
-		{
-			ptr_env = _environ[i];
-			break;
-		}
-	}
-
-	return (ptr_env + mov);
-}
-
-/**
- * _env - prints the evironment variables
- *
- * @datash: data relevant.
- * Return: 1 on success.
- */
-int _env(data_shell *datash)
-{
-	int i, j;
-
-	for (i = 0; datash->_environ[i]; i++)
-	{
-
-		for (j = 0; datash->_environ[i][j]; j++)
-			;
-
-		write(STDOUT_FILENO, datash->_environ[i], j);
-		write(STDOUT_FILENO, "\n", 1);
-	}
-	datash->status = 0;
-
-	return (1);
+int main() {
+    char cmd[MAX_CMD_LEN];
+    while (1) {
+        printf("$ ");
+        if (fgets(cmd, sizeof(cmd), stdin) == NULL) {
+            printf("\n");
+            exit(0);
+        }
+        cmd[strlen(cmd) - 1] = '\0';  // remove newline at the end
+        if (access(cmd, F_OK) != -1) {
+            char *argv[] = {cmd, NULL};
+            if (execve(cmd, argv, environ) == -1) {
+                perror("Error executing command");
+            }
+        } else {
+            printf("%s: command not found\n", cmd);
+        }
+    }
+    return 0;
 }
